16fcbc93ca23d2d41b96b27725cfd934ca7d0ea0
Yeswanth^k <epuri.yeshu@gmail.com>
2020-02-19 05:09:15 +0530

    First Check In for messey Code without SOLID.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace SOLIDPrinciple
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Welcome to the Application");
+
+            Person person = new Person();
+            System.Console.WriteLine("What is Your First Name");
+            person.FirstName = Console.ReadLine();
+            System.Console.WriteLine("What is Your Last Name");
+            person.LastName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(person.FirstName))
+            {
+                System.Console.WriteLine("You Didn't enter a valid First Name");
+                Console.ReadLine();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(person.LastName))
+            {
+                System.Console.WriteLine("You Didn't enter a valid Last Name");
+                Console.ReadLine();
+                return;
+            }
+
+            //Create a person
+            System.Console.WriteLine($"Your User Name is{person.FirstName.Substring(0,1)}, {person.LastName}");
+
         }
     }
 }
