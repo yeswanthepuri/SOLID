53e06952ca8c44e791bbf1f6595785a17b5585b9
Yeswanth^k <epuri.yeshu@gmail.com>
2020-02-19 06:10:44 +0530

    Added Solution

---
+++ SOLIDPrinciple/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SOLIDPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region S- Single Responsibility Principle
+            // StandardMessages.WelcomeMessage();
+            // var person = PesronDataCapture.Capture();
+            // bool isValid = PersonValidate.Validate(person);
+            // if (isValid)
+            // {
+            //     StandardMessages.EndApplication();
+            //     return;
+            // }
+            // //Create a person
+            // AccountGenerator.CreateAccount(person);
+            // StandardMessages.EndApplication();
+             #endregion
+             #region O - Open Close Principle
+              
+             #endregion
+        }
+
+    }
+}
